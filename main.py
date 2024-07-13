8a9cc4330631fa3cd7227744a304522200dcd444
thiagsv <thiagogabriel2901@gmail.com>
2024-07-13 20:10:22 -0300

    perf: reorganize parameters

--- main.py
+++ main.py
@@ -3,19 +3,16 @@ import datetime
 from scripts.opensim import getCalibrationDataPath, generateCalibratedModel, getTrackingDataPath, trackingMovement
 
 def main():
-    if len(sys.argv) < 2:
-        print("Usage: python main.py <mock> [<startAt> <endAt>]")
-        sys.exit(1)
+    if len(sys.argv) > 1:
+        mock = sys.argv[3] == 'True' or sys.argv[3] == '1' or sys.argv[3] == 'true'
+        realStartAt = None
+        realEndAt = None
+
+        if not mock and len(sys.argv) >= 2:
+            realStartAt = sys.argv[1]
+            realEndAt = sys.argv[2]
     
-    mock = sys.argv[1] == 'True' or sys.argv[1] == '1' or sys.argv[1] == 'true'
-    realStartAt = None
-    realEndAt = None
-
     dateTime = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-
-    if not mock and len(sys.argv) >= 4:
-        realStartAt = sys.argv[2]
-        realEndAt = sys.argv[3]
 
     getCalibrationDataPath(mock, dateTime)
     generateCalibratedModel(dateTime)
