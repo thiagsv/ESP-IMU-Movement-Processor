4a896f6392c8ac2b08f5d120002bcf2e9c6af660
thiagsv <thiagogabriel2901@gmail.com>
2024-07-13 18:35:36 -0300

    feat: create main.py

---
+++ main.py
@@ -0,0 +1,126 @@
+import opensim as osim
+from math import pi
+import sys
+
+modelPath = 'Rajagopal_2015.osim'
+orientationsFileName = 'data/'
+baseIMUName = 'pelvis_imu'
+baseIMUHeading = 'z'
+visulizeCalibration = False
+trackingFileName = 'data/mock/orientations.sto'
+modelFileName = 'calibrated_model_'
+resultsDirectory = 'IKResults'
+
+def getCalibrationDataPath(mock, unixtimestamp):
+    """
+    Retrieves the calibration data path.
+
+    Args:
+    mock (bool): Whether to use mock data.
+    unixtimestamp (str): If not mock, specify the file with data.
+
+    Returns:
+    None
+    """
+
+    global orientationsFileName
+
+    if mock:
+        orientationsFileName = orientationsFileName + 'mock/orientations.sto'
+        return
+    
+    orientationsFileName += unixtimestamp
+
+def generateCalibratedModel(unixtimestamp):
+    """
+    Generate a calibrated model
+
+    Args:
+    unixtimestamp (int)
+
+    Returns:
+    None
+    """
+    global modelFileName
+
+    sensorToOpensimRotations = osim.Vec3(-pi/2, 0, 0)
+    imuPlacer = osim.IMUPlacer()
+    imuPlacer.set_model_file(modelPath)
+    imuPlacer.set_orientation_file_for_calibration(orientationsFileName)
+    imuPlacer.set_sensor_to_opensim_rotations(sensorToOpensimRotations)
+    imuPlacer.set_base_imu_label(baseIMUName)
+    imuPlacer.set_base_heading_axis(baseIMUHeading)
+    imuPlacer.run(visulizeCalibration)
+    model = imuPlacer.getCalibratedModel()
+    model.printToXML(f'calibrated_model_{unixtimestamp}')
+
+def getTrackingDataPath(mock, unixtimestamp):
+    """
+    Retrieves the tracking data path.
+
+    Args:
+    mock (bool): Whether to use mock data.
+    unixtimestamp (str): timestamp of reading from ESP
+
+    Returns:
+    None
+    """
+    global modelFileName, orientationsFileName
+
+    modelFileName += unixtimestamp
+
+    if not mock:
+        orientationsFileName = 'data/' + unixtimestamp
+        startAt = float(startAt)  # Convert to float if necessary
+        endAt = float(endAt)      # Convert to float if necessary
+
+def trackingMovement(startAt = None, endAt = None):
+    """
+    Executes inverse kinematics tracking.
+
+    Args:
+
+    startAt (str|float): time that movement starts
+    endAt (str|float): time that movement ends
+
+    Returns:
+    None
+    """
+    sensor_to_opensim_rotation = osim.Vec3(-pi/2, 0, 0)
+    visualizeTracking = True
+
+    if startAt is None or endAt is None:
+        startAt = 7.25
+        endAt = 15
+
+
+    imuIK = osim.IMUInverseKinematicsTool()
+    imuIK.set_model_file(modelFileName)
+    imuIK.set_orientations_file(trackingFileName)
+    imuIK.set_sensor_to_opensim_rotations(sensor_to_opensim_rotation)
+    imuIK.set_results_directory(resultsDirectory)
+    imuIK.set_time_range(0, startAt)
+    imuIK.set_time_range(1, endAt)
+    imuIK.run(visualizeTracking)
+
+def main():
+    if len(sys.argv) < 3:
+        print("Usage: python main.py <mock> <unixtimestamp> [<startAt> <endAt>]")
+        sys.exit(1)
+    
+    mock = sys.argv[1] == 'True'
+    unixtimestamp = sys.argv[2]
+    realStartAt = None
+    realEndAt = None
+
+    if not mock and len(sys.argv) >= 5:
+        realStartAt = sys.argv[3]
+        realEndAt = sys.argv[4]
+
+    getCalibrationDataPath(mock, unixtimestamp)
+    generateCalibratedModel(unixtimestamp)
+    getTrackingDataPath(mock, unixtimestamp)
+    trackingMovement(realStartAt, realEndAt)
+
+if __name__ == '__main__':
+    main()
