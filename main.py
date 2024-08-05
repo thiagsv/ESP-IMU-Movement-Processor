b98a2077a57b4e9822d0aa13588db0b7e8a47f9e
thiagsv <86622333+thiagsv@users.noreply.github.com>
2024-08-05 19:32:32 -0300

    feat: start button

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import simpledialog, messagebox
 from scripts.opensim import getCalibrationDataPath, generateCalibratedModel, trackingMovement
-from scripts.esp import requestIMUData
+from scripts.esp import requestIMUData, startCollectIMUData
 from scripts.kalmanFilter import processIMUData
 
 def runSimulation(mock, startAt = None, endAt = None):
@@ -20,24 +20,35 @@ def startSimulation():
         runSimulation(mock)
 
 def getEspData():
-    url = simpledialog.askstring('Entrada', 'Digite o URL do ESP32 (e.g., http://192.168.4.1/data):')
-    if url:
-        filePath = requestIMUData(url)
+    ip = simpledialog.askstring('Entrada', 'Digite o IP do ESP32 (e.g., 192.168.4.1):')
+    if ip:
+        filePath = requestIMUData(ip)
         if filePath:
             processIMUData(filePath)
         else:
             messagebox.showerror('Erro', 'Falha ao obter dados do ESP32')
+
+def startCollecting():
+    ip = simpledialog.askstring('Entrada', 'Digite o IP do ESP32 (e.g., 192.168.4.1):')
+    if ip:
+        startCollectIMUData(ip)
+    else:
+        messagebox.showerror('Erro', 'Falha ao iniciar a coleta de dados do ESP32')    
 
 def main():
     window = tk.Tk()
     window.title('OpenSim GUI')
     window.geometry('300x150')
 
+    startCollectBtn = tk.Button(window, text='Iniciar a coleta de dados', command=startCollecting)
+    startCollectBtn.pack(pady=20)
+
     simulateBtn = tk.Button(window, text='Executar Scripts', command=startSimulation)
     simulateBtn.pack(pady=20)
 
     getEspDataBtn = tk.Button(window, text='Obter Dados do ESP32', command=getEspData)
-    getEspDataBtn.pack(pady=20)
+    getEspDataBtn.pack(pady=20)    
+    
     window.mainloop()
 
 if __name__ == '__main__':
